4df567d952fcd58b4ae332470f187c53955e11fb
Ольга Черепанова <chernichka.ekb@gmail.com>
2021-11-26 12:40:16 +0500

    Добавьте файлы проекта.

---
+++ Age/Program.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите возраст человека в диапазоне 20-69 лет:");
+            byte n = Convert.ToByte(Console.ReadLine());
+            switch (n)
+            {
+                case 20:
+                    {
+                        Console.WriteLine("Двадцать лет");
+                        break;
+                    }
+                case 21:
+                    {
+                        Console.WriteLine("Двадцать один год");
+                        break;
+                    }
+                case 22:
+                    {
+                        Console.WriteLine("Двадцать два года");
+                        break;
+                    }
+                case 23:
+                    {
+                        Console.WriteLine("Двадцать три года");
+                        break;
+                    }
+                case 24:
+                    {
+                        Console.WriteLine("Двадцать четыре года");
+                        break;
+                    }
+                case 25:
+                    {
+                        Console.WriteLine("Двадцать пять лет");
+                        break;
+                    }
+                case 26:
+                    {
+                        Console.WriteLine("Двадцать шесть лет");
+                        break;
+                    }
+                case 27:
+                    {
+                        Console.WriteLine("Двадцать семь лет");
+                        break;
+                    }
+                case 28:
+                    {
+                        Console.WriteLine("Двадцать восемь лет");
+                        break;
+                    }
+                case 29:
+                    {
+                        Console.WriteLine("Двадцать девять лет");
+                        break;
+                    }
+                case 30:
+                    {
+                        Console.WriteLine("Тридцать лет");
+                        break;
+                    }
+                case 31:
+                    {
+                        Console.WriteLine("Тридцать один год");
+                        break;
+                    }
+                case 32:
+                    {
+                        Console.WriteLine("Тридцать два года");
+                        break;
+                    }
+                case 33:
+                    {
+                        Console.WriteLine("Тридцать три года");
+                        break;
+                    }
+                case 34:
+                    {
+                        Console.WriteLine("Тридцать четыре года");
+                        break;
+                    }
+                case 35:
+                    {
+                        Console.WriteLine("Тридцать пять лет");
+                        break;
+                    }
+                case 36:
+                    {
+                        Console.WriteLine("Тридцать шесть лет");
+                        break;
+                    }
+                case 37:
+                    {
+                        Console.WriteLine("Тридцать семь лет");
+                        break;
+                    }
+                case 38:
+                    {
+                        Console.WriteLine("Тридцать восемь лет");
+                        break;
+                    }
+                case 39:
+                    {
+                        Console.WriteLine("Тридцать девять лет");
+                        break;
+                    }
+                case 40:
+                    {
+                        Console.WriteLine("Сорок лет");
+                        break;
+                    }
+                case 41:
+                    {
+                        Console.WriteLine("Сорок один год");
+                        break;
+                    }
+                case 42:
+                    {
+                        Console.WriteLine("Сорок два года");
+                        break;
+                    }
+                case 43:
+                    {
+                        Console.WriteLine("Сорок три года");
+                        break;
+                    }
+                case 44:
+                    {
+                        Console.WriteLine("Сорок четыре года");
+                        break;
+                    }
+                case 45:
+                    {
+                        Console.WriteLine("Сорок пять лет");
+                        break;
+                    }
+                case 46:
+                    {
+                        Console.WriteLine("Сорок шесть лет");
+                        break;
+                    }
+                case 47:
+                    {
+                        Console.WriteLine("Сорок семь лет");
+                        break;
+                    }
+                case 48:
+                    {
+                        Console.WriteLine("Сорок восемь лет");
+                        break;
+                    }
+                case 49:
+                    {
+                        Console.WriteLine("Сорок девять лет");
+                        break;
+                    }
+                case 50:
+                    {
+                        Console.WriteLine("Пятьдесят лет ");
+                        break;
+                    }
+                case 51:
+                    {
+                        Console.WriteLine("Пятьдесят один год");
+                        break;
+                    }
+                case 52:
+                    {
+                        Console.WriteLine("Пятьдесят два года");
+                        break;
+                    }
+                case 53:
+                    {
+                        Console.WriteLine("Пятьдесят три года");
+                        break;
+                    }
+                case 54:
+                    {
+                        Console.WriteLine("Пятьдесят четыре года");
+                        break;
+                    }
+                case 55:
+                    {
+                        Console.WriteLine("Пятьдесят пять лет");
+                        break;
+                    }
+                case 56:
+                    {
+                        Console.WriteLine("Пятьдесят шесть лет");
+                        break;
+                    }
+                case 57:
+                    {
+                        Console.WriteLine("Пятьдесят семь лет");
+                        break;
+                    }
+                case 58:
+                    {
+                        Console.WriteLine("Пятьдесят восемь лет");
+                        break;
+                    }
+                case 59:
+                    {
+                        Console.WriteLine("Пятьдесят девять лет");
+                        break;
+                    }
+                case 60:
+                    {
+                        Console.WriteLine("Шестьдесят лет");
+                        break;
+                    }
+                case 61:
+                    {
+                        Console.WriteLine("Шестьдесят один год");
+                        break;
+                    }
+                case 62:
+                    {
+                        Console.WriteLine("Шестьдесят два года");
+                        break;
+                    }
+                case 63:
+                    {
+                        Console.WriteLine("Шестьдесят три года");
+                        break;
+                    }
+                case 64:
+                    {
+                        Console.WriteLine("Шестьдесят четыре года");
+                        break;
+                    }
+                case 65:
+                    {
+                        Console.WriteLine("Шестьдесят пять лет");
+                        break;
+                    }
+                case 66:
+                    {
+                        Console.WriteLine("Шестьдесят шесть лет");
+                        break;
+                    }
+                case 67:
+                    {
+                        Console.WriteLine("Шестьдесят семь лет");
+                        break;
+                    }
+                case 68:
+                    {
+                        Console.WriteLine("Шестьдесят восемь лет");
+                        break;
+                    }
+                case 69:
+                    {
+                        Console.WriteLine("Шестьдесят девять лет");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Ошибка!");
+                    }
+                    break;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Rectangle/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты первой вершины прямоугольника");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите координаты второй вершины прямоугольника");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            int x3 = 0;
+            int y3 = 0;
+            if (x1 != x2 || y1 != y2)
+            {
+                Console.WriteLine("Введите координаты третьей вершины прямоугольника");
+                x3 = Convert.ToInt32(Console.ReadLine());
+                y3 = Convert.ToInt32(Console.ReadLine());
+            }
+            else
+            {
+                Console.WriteLine("Координаты введены неверно. Невозможно построить прямоугольник со сторонами, параллельными осям координат");
+            }
+            int x4;
+            int y4;
+            if (x3 == x1 && y3 == y2)
+            {
+                x4 = x2;
+                y4 = y1;
+                Console.WriteLine("Координаты четвертой вершины прямоугольника {0}, {1}", x4, y4);
+            }
+            else if (x3 == x2 && y3 == y1)
+            {
+                x4 = x1;
+                y4 = y2;
+                Console.WriteLine("Координаты четвертой вершины прямоугольника {0}, {1}", x4, y4);
+            }
+            else if (x3 != x1 && x3 != x2 && y3 == y1)
+            {
+                x4 = x3;
+                y4 = y2;
+                Console.WriteLine("Координаты четвертой вершины прямоугольника {0}, {1}", x4, y4);
+            }
+            else if (x3 != x1 && x3 != x2 && y3 == y2)
+            {
+                x4 = x3;
+                y4 = y1;
+                Console.WriteLine("Координаты четвертой вершины прямоугольника {0}, {1}", x4, y4);
+            }
+            else if (y3 != y1 && y3 != y2 && x3 == x1)
+            {
+                x4 = x2;
+                y4 = y3;
+                Console.WriteLine("Координаты четвертой вершины прямоугольника {0}, {1}", x4, y4);
+            }
+            else if (y3 != y1 && y3 != y2 && x3 == x2)
+            {
+                x4 = x1;
+                y4 = y3;
+                Console.WriteLine("Координаты четвертой вершины прямоугольника {0}, {1}", x4, y4);
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
